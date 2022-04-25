0ce0f0fab6aa87d252323f0df07238d0f39cb19d
Filip Ruisl <33035240+filipruisl@users.noreply.github.com>
2022-04-25 22:10:45 +0200

    Squares

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="TouchSurface"></div>
-      </header>
+      <div className="TouchSurface1">
+        <div className="Pointer"></div>
+      </div>
+      <div className="TouchSurface2">
+        <div className="Pointer"></div>
+      </div>
     </div>
   );
 }
