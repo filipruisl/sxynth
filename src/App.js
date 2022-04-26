1fa18ddf1f92d4c13c14473fc0c4b2c8dad7b36f
Filip Ruisl <33035240+filipruisl@users.noreply.github.com>
2022-04-26 09:37:24 +0200

    Added 2 touchareas, each on half of the screen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Wrapper">
       <div className="TouchSurface1">
         <div className="Pointer"></div>
       </div>
